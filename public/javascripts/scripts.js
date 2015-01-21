adc7e2fe12a13c721b31cd6047396182027ee2dc
PalermoD <palermo.deschamps@gmail.com>
2015-01-21 16:58:59 -0500

    finished

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
    // set options
    var speed = 500;  //fade speed 
    var autoswitch = true;  // auto slider option
-   var autoswith_speed = 4000;// auto slider speed
+   var autoswitch_speed = 4000;// auto slider speed
      
 
      // add active class
@@ -16,8 +16,19 @@ $(document).ready(function(){
      //show first slide 
      $('.active').show();
 
-     $('#next').on('click', function(){
-         $('.active').removeClass('active').addClass('oldActive');
+     $('#next').on('click', nextSlide);
+
+
+     $('#prev').on('click', prevSlide);
+
+     //Auto Slider
+     if(autoswitch == true){
+     	setInterval(nextSlide,autoswitch_speed);
+     }
+
+     //Swith next slide
+     function nextSlide(){
+     	$('.active').removeClass('active').addClass('oldActive');
          if($('.oldActive').is(':last-child')){
          	$('.slide').first().addClass('active');
          } else {
@@ -26,6 +37,20 @@ $(document).ready(function(){
          $('.oldActive').removeClass('oldActive');
          $('.slide').fadeOut(speed);
          $('.active').fadeIn(speed);
-     });
+
+     }
+
+     function prevSlide(){
+     	$('.active').removeClass('active').addClass('oldActive');
+         if($('.oldActive').is(':first-child')){
+         	$('.slide').last().addClass('active');
+         } else {
+         	$('.oldActive').prev().addClass('active');
+         }
+         $('.oldActive').removeClass('oldActive');
+         $('.slide').fadeOut(speed);
+         $('.active').fadeIn(speed);
+     }
+   
 
 });
